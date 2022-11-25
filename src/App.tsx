588a4cd41dad02087f676f03254e2a38c16bc785
Timofei Kurohtin <forgwent@gmail.com>
2022-11-26 01:46:31 +0300

    Try fix url

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ const App: Component = () => {
     <div class="h-screen p-4 main-menu-bg">
       <div class=" h-full border-4 border-purple-900 flex items-center justify-center">
         <div class="flex flex-col items-center animate-slow-shake">
-          <img src="src/assets/skull.png" class="h-24 mb-4" />
+          <img
+            src={new URL('"src/assets/skull.png"', import.meta.url).href}
+            class="h-24 mb-4"
+          />
           <h1 class="text-7xl text-yellow-400 drop-shadow font-GothicPixels font-extralight mb-4 before:content-['Tenebris'] before:absolute before:text-black before:-z-10 before:left-1 before:top-1">
             Tenebris
           </h1>
